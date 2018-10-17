8e27a080fae7e1f88988fe2c361f7e754231939a
Carhoglu <carhoglu_92_bjk@hotmail.com>
2018-10-17 14:31:40 +0300

    Pipes start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LibraryComponent } from './library/library.component';
@@ -7,13 +7,15 @@ import { BooksComponent } from './books/books.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MembersComponent } from './members/members.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 const appRoutes: Routes = [
- { path: '' , redirectTo : 'library' , pathMatch : 'full'},
- { path: 'library' , component : LibraryComponent },
- { path: 'books' , component : BooksComponent },
- { path: 'members' , component : MembersComponent },
- { path: '**' , component : LibraryComponent }
+  { path: '', redirectTo: 'library', pathMatch: 'full' },
+  { path: 'library', component: LibraryComponent },
+  { path: 'books', component: BooksComponent },
+  { path: 'members', component: MembersComponent },
+  { path: 'pipes', component: PipesComponent },
+  { path: '**', component: LibraryComponent }
 ];
 
 @NgModule({
@@ -21,7 +23,8 @@ const appRoutes: Routes = [
     AppComponent,
     LibraryComponent,
     BooksComponent,
-    MembersComponent
+    MembersComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
@@ -32,4 +35,4 @@ const appRoutes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
