986126d92e9c708486265cf0d199b41ddcc3b538
Carhoglu <carhoglu_92_bjk@hotmail.com>
2018-10-18 10:58:30 +0300

    Add Headers Post Request

--- src/app/http/server.service.ts
+++ src/app/http/server.service.ts
@@ -1,12 +1,13 @@
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 @Injectable()
 export class ServerService {
    constructor(private http : Http) {
    }
-
+  
    store(servers : any []) {
-    return this.http.post('https://angular-udemy-http-ca329.firebaseio.com/data.json',servers);
+    const headers = new Headers({'Content-Type': 'application/json'});
+    return this.http.post('https://angular-udemy-http-ca329.firebaseio.com/data.json',servers,{ headers: headers});
    }
   
   
